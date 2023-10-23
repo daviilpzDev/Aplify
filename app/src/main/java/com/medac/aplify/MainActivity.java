b28867e7746e47d5459acf2a909c4267b70df7ac
daviilpz <davidlopez00@proton.me>
2023-10-23 14:53:31 +0200

    1.1

--- app/src/main/java/com/medac/aplify/MainActivity.java
+++ app/src/main/java/com/medac/aplify/MainActivity.java
@@ -2,7 +2,9 @@ package com.medac.aplify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void initSecAct(View view){
+
+        //Intent intentSecondary = new Intent(this, Secondary_activity.class);
+        //startActivity( intentSecondary);
     }
 
 
