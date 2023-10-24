2955fd154a0c1dd8df3dd168760bb7e665820d87
daviilpz <davidlopez00@proton.me>
2023-10-24 17:55:45 +0200

    Lista la conexion entre las 3 interfaces

--- app/src/main/java/com/medac/aplify/forgetPasword_activity.java
+++ app/src/main/java/com/medac/aplify/forgetPasword_activity.java
@@ -2,13 +2,23 @@ package com.medac.aplify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 
 public class forgetPasword_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_forget_pasword);
+
+    }
+
+    public void goToMainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
