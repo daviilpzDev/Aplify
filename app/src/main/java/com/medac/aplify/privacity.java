f656ac6a07ba20d0925b8e24eeb147de3706b435
Calero0064 <122987572+Calero0064@users.noreply.github.com>
2023-10-25 18:22:55 +0200

    Terminos y Privacidad

---
+++ app/src/main/java/com/medac/aplify/privacity.java
@@ -0,0 +1,14 @@
+package com.medac.aplify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class privacity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.privacity_activity);
+    }
+}
