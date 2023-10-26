69f22a511088bf51ed9a1749a0eaaddae4a3785e
daviilpzDev <davidlopez00@proton.me>
2023-10-26 17:29:19 +0200

    falta la conexion de privacity y los saltos de linea

--- app/src/main/java/com/medac/aplify/forgetPasword_activity.java
+++ app/src/main/java/com/medac/aplify/forgetPasword_activity.java
@@ -19,7 +19,7 @@ public class forgetPasword_activity extends AppCompatActivity {
     
     // Llamada de vuelta al main
     public void goToMainActivity(View view) {
-        Intent intent = new Intent(this, privacity.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
