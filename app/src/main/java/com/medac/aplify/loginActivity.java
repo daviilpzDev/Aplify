1be98f0215d856cbf0e4ffae8cf59ab54a18ffac
Calero0064 <122987572+Calero0064@users.noreply.github.com>
2023-10-31 10:11:39 +0100

    Login por google

--- app/src/main/java/com/medac/aplify/loginActivity.java
+++ app/src/main/java/com/medac/aplify/loginActivity.java
@@ -1,18 +1,33 @@
 package com.medac.aplify;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 
 public class loginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private GoogleSignInClient googleSignInClient;
+    private FirebaseAuth firebaseAuth;
+    private static final int RC_SIGN_IN = 123; // Código de solicitud para iniciar sesión con Google
     @Override
     // Inicio de app metodo onCreate
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +38,65 @@ public class loginActivity extends AppCompatActivity {
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
+        // Inicializa FirebaseAuth y GoogleSignInOptions
+        firebaseAuth = FirebaseAuth.getInstance();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Encuentra la ImageView en tu diseño
+        ImageView googleSignInButton = findViewById(R.id.imageView4);
+
+        // Agrega un OnClickListener para la ImageView
+        googleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signInWithGoogle();
+            }
+        });
     }
+
+    private void signInWithGoogle() {
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // El inicio de sesión con Google falló
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // El inicio de sesión con Google fue exitoso, aquí puedes realizar acciones adicionales.
+                            // Por ejemplo, puedes redirigir al usuario a la actividad principal de la aplicación.
+                        } else {
+                            // El inicio de sesión con Google falló
+                        }
+                    }
+                });
+    }
+
+
+
 
 
 
